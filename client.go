8925001f2617bd958213939f5e53dd69748147c1
Vladimir Sushkov <vvsushkov@edu.hse.ru>
2023-03-25 16:05:09 +0300

    Struct definition

---
+++ client.go
@@ -0,0 +1,34 @@
+package irc_client
+
+import "time"
+
+type (
+	lseq uint64
+	URI  string
+)
+
+type Message struct {
+	MessageId lseq
+	ParentId  lseq
+	UserId    string
+	Text      string
+	Timestamp time.Time
+}
+
+type Transport interface {
+	SendMessages(dest URI, msgs []Message) error
+	ReceiveMessages(src URI, lastReceived lseq) ([]Message, error)
+}
+
+type client struct {
+}
+
+func (c *client) SendMessages(dest URI, msgs []Message) error {
+	//TODO
+	panic("implement me")
+}
+
+func (c *client) ReceiveMessages(src URI, lastReceived lseq) ([]Message, error) {
+	//TODO
+	panic("implement me")
+}
