a46f0cc3fc6bacffcdbbfeb82f6e58a5fff185d5
Vladimir Sushkov <vvsushkov@edu.hse.ru>
2023-03-26 16:26:11 +0300

    And receiving

--- irc_transport_test.go
+++ irc_transport_test.go
@@ -10,7 +10,8 @@ import (
 type IRCTransportTestSuite struct {
 	suite.Suite
 
-	client Transport
+	client1 Transport
+	client2 Transport
 }
 
 func TestIRCTransportTestSuite(t *testing.T) {
@@ -19,21 +20,30 @@ func TestIRCTransportTestSuite(t *testing.T) {
 
 func (s *IRCTransportTestSuite) SetupSuite() {
 	var err error
-	s.client, err = NewIRCTransport("test_user")
+	s.client1, err = NewIRCTransport("test_user_1")
+	s.Require().NoError(err)
+	s.client2, err = NewIRCTransport("test_user_2")
 	s.Require().NoError(err)
 }
 
 func (s *IRCTransportTestSuite) TestSendMessages() {
-	time.Sleep(5 * time.Second)
-	err := s.client.SendMessages("error1707_", []Message{
+	msgs, err := s.client2.ReceiveMessages("test_user_1", 0)
+	s.Require().NoError(err)
+	s.Assert().Len(msgs, 0)
+
+	err = s.client1.SendMessages("test_user_2", []Message{
 		{
 			MessageId: 1,
 			ParentId:  1,
-			UserId:    "test_user",
+			UserId:    "test_user_2",
 			Text:      "some text",
 			Timestamp: time.Now(),
 		},
 	})
 	s.Require().NoError(err)
-	time.Sleep(20 * time.Second)
+	time.Sleep(2 * time.Second)
+
+	msgs, err = s.client2.ReceiveMessages("test_user_1", 0)
+	s.Require().NoError(err)
+	s.Assert().Len(msgs, 1)
 }
