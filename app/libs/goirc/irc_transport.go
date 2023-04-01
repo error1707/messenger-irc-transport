a706ec3bc77caa5c7492f0d571aed21ff06b1223
Vladimir Sushkov <vvsushkov@edu.hse.ru>
2023-04-01 10:06:06 +0300

    Add docs

--- app/libs/goirc/irc_transport.go
+++ app/libs/goirc/irc_transport.go
@@ -15,6 +15,8 @@ import (
 const MessageCommand = "PRIVMSG"
 const MessageReceiveBufferSize = 32
 
+// IrcTransport is client for sending and receiving messages via IRC protocol.
+// It uses one of dal.net servers to transmit any text data.
 type IrcTransport struct {
 	ircClient       *irc.Client
 	defaultPrefix   *irc.Prefix
@@ -23,6 +25,7 @@ type IrcTransport struct {
 	initialized chan struct{}
 }
 
+// NewIRCTransport creates and initializes IrcTransport client with specified username.
 func NewIRCTransport(username string) (*IrcTransport, error) {
 	conn, err := net.Dial("tcp", "bitcoin.uk.eu.dal.net:6667")
 	if err != nil {
@@ -62,6 +65,7 @@ func NewIRCTransport(username string) (*IrcTransport, error) {
 	return transport, nil
 }
 
+// SendMessages send test message to dest which is username of another user
 func (t *IrcTransport) SendMessages(dest string, msg string) error {
 	return t.ircClient.WriteMessage(&irc.Message{
 		Prefix:  t.defaultPrefix.Copy(),
@@ -72,14 +76,19 @@ func (t *IrcTransport) SendMessages(dest string, msg string) error {
 	})
 }
 
+// StartReceiveMessagesFrom creates listener for messages from another user by username.
+// Should be called once before any GetMessageFrom call.
 func (t *IrcTransport) StartReceiveMessagesFrom(src string) {
-	t.receiveChannels.Store(src, make(chan string, MessageReceiveBufferSize))
+	t.receiveChannels.LoadOrStore(src, make(chan string, MessageReceiveBufferSize))
 }
 
+// StopReceiveMessagesFrom removes listener for messages from another user by username.
 func (t *IrcTransport) StopReceiveMessagesFrom(src string) {
 	t.receiveChannels.Delete(src)
 }
 
+// GetMessageFrom returns one message from message listener by username or [io.EOF] if no messages available.
+// If no listener where created returns error.
 func (t *IrcTransport) GetMessageFrom(src string) (string, error) {
 	msgChan, ok := t.receiveChannels.Load(src)
 	if !ok {
